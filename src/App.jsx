2a8839784e8874e6f3aabe68dff2b735f4233726
Adarsh Verma <adarshverma549@gmail.com>
2023-06-05 17:55:23 +0530

    file - main.jsx and App.jsx important file

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import DetailSection from "./Components/DetailSection";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import HeroSection from "./Components/HeroSection";
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <HeroSection/>
+      <DetailSection/>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
